3980747c8d7a4d463dda10a77e2197d495281390
cathyfu1215 <phoenixfucong@gmail.com>
2024-07-04 14:40:51 -0700

    Added useState

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import { TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
 
   const appName = 'Cathy\'s summer project';
+  const[text, setText] = useState('');
   return (
     <View style={styles.container}>
       <Header name = {appName} theme="dark">
-        <Text>child1</Text>
-        <Text>child2</Text>
       </Header>
       <Text>Welcome to {appName}(I am a text)</Text>
+      <TextInput 
+      style={{height:40}}
+      placeholder="Enter something here..."
+      onChangeText={newText => setText(newText)}
+      value={text}
+      autoCapitalize={true}
+       />
+       <Text>your input is: {text}</Text>
       <StatusBar style="auto" />
     </View>
   );
