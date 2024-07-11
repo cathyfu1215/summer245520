f4d1ff66b33d1f6f0deb9ff6e63aac08a708ca35
cathyfu1215 <phoenixfucong@gmail.com>
2024-07-11 13:57:16 -0700

    added list rendering

--- App.js
+++ App.js
@@ -6,15 +6,26 @@ import Input from './components/Input';
 import { SafeAreaView } from 'react-native';
 
 
+
 export default function App() {
-  const [receivedText,setReceivedText] = useState(''); 
+  const [goals,setGoals] = useState([]); 
   const [modalVisible, setModalVisible] = useState(false);
   const appName = 'Cathy\'s summer project';
 
   //to receive data
   function handleInputData(data){
     console.log('data we got(the username):',data);
-    setReceivedText(data);
+    //setReceivedText(receivedText.concat(data));
+
+    //define a new object{text:data, id:random number}
+    const newGoal = {text:data, id:Math.random()};
+    const newArray = [...goals, newGoal];
+
+    
+    setGoals((goals)=>{
+      return [...goals, newGoal]
+    });
+
   }
 
   function handleModalVisible(){
@@ -36,7 +47,10 @@ export default function App() {
       <Input focused = {true} message='Thank you for entering the input' handleInputData={handleInputData}
              modalVisible={modalVisible} handleModalNotVisible={handleModalNotVisible}/>
       <View style={styles.bottomContainer}>
-      <Text style={styles.textStyle}>{receivedText}</Text>
+      <Text style={styles.textStyle}>Your Goals:</Text>
+      {goals.map((goal)=>{
+        return <Text key={goal.id} style={styles.textStyle}>{goal.text}</Text>
+      })}
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
