9c04f50f126584422575e5a26f8ea5402d74bbec
cathyfu1215 <phoenixfucong@gmail.com>
2024-07-16 14:16:01 -0700

    refactoring styles

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { styleHelper } from '../styleHelper'
 
 export default function Header(props) {
   return (
     <View>
     <View style={{borderRadius: 20}}>
-      <Text style={{padding:10, fontSize:22, 
-                   backgroundColor:"lightblue", borderColor:'gold', 
-                   borderWidth:2, padding: 5, margin:5}}>
+      <Text style={styleHelper.headerCompStyle}>
                     Welcome to {props.name}</Text>
       {props.children}
     </View>
