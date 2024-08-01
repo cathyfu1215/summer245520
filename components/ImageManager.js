f97ef89e458ee989eb04dfa74ba9151e98d24f88
cathyfu1215 <phoenixfucong@gmail.com>
2024-08-01 10:35:37 -0700

    added preview

--- components/ImageManager.js
+++ components/ImageManager.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
 import PressableButton  from './PressableButton';
 import * as ImagePicker from 'expo-image-picker';
 import Feather from '@expo/vector-icons/Feather';
+import { Image } from 'react-native';
+import { useState } from 'react';
 
 
 
 function ImageManager() {
     const [status, requestPermission] = ImagePicker.useCameraPermissions();
-
+    const [imageURI, setImageURI] = useState(null);
+  
     const vefifyPermissions = async() => {
         if(status.granted === true) {
             return true;
@@ -27,6 +30,10 @@ function ImageManager() {
             vefifyPermissions();
             const result = await ImagePicker.launchCameraAsync({});
             console.log('result from camera',result);
+            if (!result.canceled) {
+                console.log('photo uri',result.assets[0].uri);
+                setImageURI(result.assets[0].uri);
+            }
         }
         catch (err) {
             console.log(err);
@@ -39,6 +46,7 @@ function ImageManager() {
         <PressableButton pressedFunction={takeImageHandler}>
         <Feather name="camera" size={24} color="black" />
         </PressableButton>
+        <Image style={{height:100,width:100}}source={{uri: imageURI}}/>
     </View>
   )
 }
