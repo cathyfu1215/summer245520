b4c07620541bc1d8f1f22a75735ab93fc443ea4b
cathyfu1215 <phoenixfucong@gmail.com>
2024-08-04 17:15:05 -0700

    added mapview and marker

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,41 @@
-import React from 'react'
-import MapView from "react-native-maps";
-import {Text, View } from 'react-native';
-
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { Text, View } from 'react-native';
+import { useState } from 'react';
 
 function Map() {
-  return (
-   <View>
-    <Text>map</Text>
-    <MapView style={{flex:1}}/>
-   </View>
-  )
+    const [selectedLocation, setSelectedLocation] = useState(
+        // {
+        // latitude: 49.281348881743014,
+        // longitude: -123.11445562981213,
+        // latitudeDelta: 0.0922,
+        // longitudeDelta: 0.0421,
+        // }
+);
+
+    return (
+        <View style={{ flex: 1, margin: 10}}>
+            
+            <MapView
+                style={{ flex: 1, height: 300, width: 350 }}
+                initialRegion={{
+                    latitude: 49.281348881743014,
+                    longitude: -123.11445562981213,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                onPress={(event) => {
+                    console.log('event', event.nativeEvent)
+                    setSelectedLocation({
+                        latitude: event.nativeEvent.coordinate.latitude,
+                        longitude: event.nativeEvent.coordinate.longitude,
+                    });
+                }}
+            >
+                {selectedLocation?<Marker coordinate={{ latitude: selectedLocation.latitude, longitude: selectedLocation.longitude }} />:null}
+            </MapView>
+        </View>
+    );
 }
 
-export default Map
+export default Map;
